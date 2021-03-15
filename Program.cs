02aa55030a7da1454c7c7188cd3b8a0c22b04958
00190220 <00100220@uca.edu.sv>
2021-03-15 17:57:15 -0600

    Modify: Program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace program01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var a = 0;
+           var b = 0;
+
+           Console.WriteLine("Enter a value: ");
+           a = Int32.Parse(Console.ReadLine());
+
+           Console.WriteLine("Enter anothere value: ");
+           b = Int32.Parse(Console.ReadLine());
+
+           Console.WriteLine($"Adding the values of {a} and {b} give: ");
+
+           
         }
     }
 }
